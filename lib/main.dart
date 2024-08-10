a11ce38184b2ab1bc7205921c254c736d593efda
DevNadimm <nadimchowdhury87@gmail.com>
2024-08-10 12:48:23 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: backgroundColor
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:open_wall/views/screens/search_screen.dart';
 import 'package:open_wall/views/widgets/category_block.dart';
+import '../constants/app_colors.dart';
 import '../widgets/custom_app_bar.dart';
 import '../widgets/grid_container.dart';
-import '../widgets/search_bar_container.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +17,66 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            const CustomAppBar(word1: 'Open', word2: 'Wall', showBackButton: false),
-            SearchBarContainer(),
+            const CustomAppBar(
+                word1: 'Open', word2: 'Wall', showBackButton: false),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Container(
+                padding: const EdgeInsets.only(left: 16),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: primaryColor.withOpacity(0.4), width: 2),
+                  color: primaryColor.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        cursorColor: primaryColor,
+                        decoration: const InputDecoration(
+                          hintText: 'Search...',
+                          hintStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: mainTextColor,
+                          ),
+                          border: InputBorder.none,
+                        ),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: mainTextColor,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        if (searchController.text.trim().isNotEmpty) {
+                          Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                              builder: (_) => SearchScreen(
+                                query: searchController.text,
+                              ),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Please enter a search query'),
+                            ),
+                          );
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.search_rounded,
+                        color: mainTextColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             const CategoryBlock(),
             const GridContainer(),
           ],
